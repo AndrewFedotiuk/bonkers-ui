b2c9575d6cb312aab8f85fb9f5c45bd3ae57b769
Andrew <andrii.fedotiuk@bonkers.ie>
2022-06-22 10:51:35 +0100

    update(*): linting

--- src/components/ui-button/ui-button.stories.ts
+++ src/components/ui-button/ui-button.stories.ts
@@ -1,39 +1,38 @@
-import UiButton from './ui-button.vue';
+import UiButton from "./ui-button.vue";
 
 export default {
-    title: 'Components/ui-button',
-    component: UiButton,
-    // More on argTypes: https://storybook.js.org/docs/vue/api/argtypes
-    argTypes: {
-        classes: {
-            control: { type: 'select' },
-            options: ['small', 'medium', 'large'],
-            description: 'The button size',
-        },
-        slot: {
-            control: { type: 'text' },
-            description: 'The slot text or component',
-        }
-    },
-    args:{
-        slot: 'default text',
-        classes: 'medium',
-    },
+	title: "Components/ui-button",
+	component: UiButton,
+	// More on argTypes: https://storybook.js.org/docs/vue/api/argtypes
+	argTypes: {
+		classes: {
+			control: { type: "select" },
+			options: ["small", "medium", "large"],
+			description: "The button size",
+		},
+		slot: {
+			control: { type: "text" },
+			description: "The slot text or component",
+		}
+	},
+	args:{
+		slot: "default text",
+		classes: "medium",
+	},
 };
 
 type MyComponentProps = InstanceType<typeof UiButton>["$props"];
 
 const Template = (args: MyComponentProps) => ({
-    // Components used in your story `template` are defined in the `components` object
-    components: { UiButton },
-    // The story's `args` need to be mapped into the template through the `setup()` method
-    setup() {
-        return { args };
-    },
-    // And then the `args` are bound to your component with `v-bind="args"`
-    template: `<ui-button v-bind="args">${args.slot}</ui-button>`,
+	// Components used in your story `template` are defined in the `components` object
+	components: { UiButton },
+	// The story's `args` need to be mapped into the template through the `setup()` method
+	setup() {
+		return { args };
+	},
+	// And then the `args` are bound to your component with `v-bind="args"`
+	template: `<ui-button v-bind="args">${args.slot}</ui-button>`,
 });
-
 
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/vue/writing-stories/args
